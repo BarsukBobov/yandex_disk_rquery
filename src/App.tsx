e7904bd7319eecf1d41bda5a2daf6a772424e1f0
MII <igoryha-701@mail.ru>
2023-08-14 06:58:50 +0500

    яндекс виджет

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import YandexDiskPage from "./pages/YandexDiskPage";
 import AuthorizePage from "./pages/AuthorizePage";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,11 +9,10 @@ function App() {
 
   return (
       <Routes>
-        <Route path ="/" element={<HomePage />} />
         <Route path ="/login" element={<AuthorizePage/>}/>
           <Route path ="/redirect" element={<RedirectPage/>}/>
         <Route path ="/YandexDisk" element={<YandexDiskPage/>}/>
-        <Route path="*" element={<HomePage />} />
+        <Route path="*" element={<AuthorizePage />} />
       </Routes>
   );
 }
